f7d786463172bc475de1e4be5cd4c8e4dbeb2898
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2022-12-25 13:34:51 +0100

    Updates

---
+++ precache-manifest.9fe6fd513eb84d8c25bd0cefb429fcb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "584b3d9d57d060fbc532",
+    "url": "/monsters/static/css/main.9ae5146b.chunk.css"
+  },
+  {
+    "revision": "584b3d9d57d060fbc532",
+    "url": "/monsters/static/js/main.9646d05a.chunk.js"
+  },
+  {
+    "revision": "3c8a8ee0bd2795148260",
+    "url": "/monsters/static/js/runtime~main.b27affc0.js"
+  },
+  {
+    "revision": "f8d3de7004038200ba2c",
+    "url": "/monsters/static/js/2.74898a25.chunk.js"
+  },
+  {
+    "revision": "278bb1a1e59d10282b78",
+    "url": "/monsters/static/js/3.3f8aa67b.chunk.js"
+  },
+  {
+    "revision": "5230f1b657caece94ab7f9db85f2244a",
+    "url": "/monsters/index.html"
+  }
+];
